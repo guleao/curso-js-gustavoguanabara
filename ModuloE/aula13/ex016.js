9fb58d0a4a528095fe08db6b0a9d9b453894a5d6
guleao <guztavusz@gmail.com>
2024-01-25 13:10:20 -0300

    Aula 13 finalizada

---
+++ ModuloE/aula13/ex016.js
@@ -0,0 +1,30 @@
+// Verificação no final:
+
+/*var c = 1
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while ( c <= 6)*/
+
+
+// Verificação no ínicio:
+
+/*var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+
+// Modo força bruta mega neandertal
+
+/*
+console.log('Tudo bem?)
+console.log('Tudo bem?)
+console.log('Tudo bem?)
+console.log('Tudo bem?)
+console.log('Tudo bem?)
+console.log('Tudo bem?)
+console.log('Tudo bem?)
+*/
