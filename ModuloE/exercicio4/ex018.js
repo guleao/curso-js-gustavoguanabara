9bea87461055fca5f4650d5d01cd0f7ecce22970
guleao <guztavusz@gmail.com>
2024-01-25 14:52:52 -0300

    ModuloE FINALIZADO

--- ModuloE/exercicio4/ex018.js
+++ ModuloE/exercicio4/ex018.js
@@ -1,7 +1,27 @@
-function contar(){
-    
-    var numInicio = document.getElementById('inicio')
-    var numFinal = document.getElementById('final')
+function contar() {
+    let ini = document.getElementById('inicio')
+    let fim = document.getElementById('final')
+    let passo = document.getElementById('passo')
+    var res = document.getElementById('res')
 
-    
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ){
+        res.innerHTML = 'Impossivel contar'
+    } else {
+        res.innerHTML = 'Contando...'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(i < f){
+            //Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}` 
+            }
+        } else {
+            // Contagem decrescente
+            for(let c = i; c>= f; c-= p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += ` \u{1F911}`
+    }
 }
